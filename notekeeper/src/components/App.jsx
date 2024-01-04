977459d3979f23917f3d26358766ba71ade32dae
Ulugbek_Leo <sherovulugbek04@gmail.com>
2024-01-04 20:33:47 +0500

    Created component files

---
+++ notekeeper/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+
+
+export default function App() {
+  return (
+    <div>
+        <Header />
+        <Note />
+        <Footer />
+    </div>
+  )
+}
