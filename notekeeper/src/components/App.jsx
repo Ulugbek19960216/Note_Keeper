d87d4dde1a8a046b7cca90269fe6f8c170f72d78
Ulugbek_Leo <sherovulugbek04@gmail.com>
2024-01-18 21:29:36 +0500

    Added functionality of add and delete

--- notekeeper/src/components/App.jsx
+++ notekeeper/src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
@@ -7,13 +7,50 @@ import CreateArea from "./CreateArea";
 
 
 export default function App() {
- 
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote]
+    });
+  }
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter( (noteItem, index) => {
+        return index !== id;
+      })
+    })
+  }
+
   return (
     <div>
         <Header />
-        <CreateArea />
-        <Note key={1} title="Note title" content="Note content" />
+        <CreateArea onAdd={addNote}/>
+        {notes.map( (item, index) => {
+          return <Note 
+            key={index}
+            id={index}
+            title={item.title}
+            content={item.content}
+            onDelete={deleteNote}
+          />
+        })}
         <Footer />
     </div>
   )
 }
+
+
+
+
+// Challange:
+//1 Implement the add note functionality.
+//- Create a constant that keeps track of the title and content.
+//- Pass the new note back to the App.
+//- Add new note to an array.
+//- Take array and render seperate Note componenets for each item.
+
+//2- Implement the delete note functionality.
+//- Callback from the Note component to trigger a delete function.
+//- Use the filter function to filter out item that needs deletation
+//- Pass a id over to the Node component, pass it back to the App when deleting.
--- notekeeper/src/components/CreateArea.jsx
+++ notekeeper/src/components/CreateArea.jsx
@@ -1,13 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 
 
-function CreateArea() {
+function CreateArea(props) {
+const [note, setNote] = useState({
+    title: "",
+    content: ""
+})
+
+ 
+function handleChange(event) {
+    const {name, value} = event.target;
+
+    setNote(prevNote => {
+        return {
+            ...prevNote,
+            [name]: value
+        };
+    });
+
+}
+
+function addForm(event){
+    props.onAdd(note)
+    setNote({
+        title: "",
+        content: ""
+    })
+    event.preventDefault();
+}
+
     return (
         <div>
             <form>
-                <input name="title" placeholder="Title"/>
-                <textarea name="content" placeholder="Take a note..." rows="3"/>
-                <button>Add</button>
+                <input 
+                    onChange={handleChange}
+                    value={note.title}
+                    name="title" 
+                    placeholder="Title"/>
+                <textarea 
+                    onChange={handleChange}
+                    value={note.content}
+                    name="content" 
+                    placeholder="Take a note..." rows="3"/>
+                <button 
+                    onClick={addForm}>Add</button>
             </form>
         </div>
     )
